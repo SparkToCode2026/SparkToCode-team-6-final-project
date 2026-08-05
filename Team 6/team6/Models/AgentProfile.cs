f4daa75f5b38d18b5d7abd7b35d762bc0e8c5700
AL-WALEED AL-HILALI <wk3042711@gmail.com>
2026-08-05 11:19:58 +0400

    add user and agent profile models

---
+++ Team 6/team6/Models/AgentProfile.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace team6.Models
+{
+    public class AgentProfile
+    {
+        [Key]
+        public int AgentProfileId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LicenseNumber { get; set; } = string.Empty;
+
+        [MaxLength(1000)]
+        public string? Bio { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Phone { get; set; } = string.Empty;
+
+        // Foreign Key -> User (1-to-1)
+        [Required]
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+    }
+}
--- Team 6/team6/Models/User.cs
+++ Team 6/team6/Models/User.cs
@@ -1,6 +1,29 @@
-﻿namespace team6.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace team6.Models
 {
     public class User
     {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(150)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(20)]
+        public string Role { get; set; } = "Client"; // "Client", "Agent", or "Admin"
+
+        // Navigation property: one User can have one AgentProfile (only if Role == "Agent")
+        public AgentProfile? AgentProfile { get; set; }
     }
 }
